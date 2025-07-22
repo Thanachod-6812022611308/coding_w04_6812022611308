556f4e22148f2602c86119eab1fc7f005c4c3e57
Thanachod-6812022611308 <s6812022611308@email.kmutnb.ac.th>
2025-07-22 15:17:27 +0700

    Delete 6812022611308 compro/coding_w04-01.c

--- 6812022611308 compro/coding_w04-01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>          //input/output
-
-int main() {                // จุดเริ่มต้นโปรแกรม
-    printf("Hello,\n");     // ขึ้นคำว่า Hello
-    printf("C Tokens!\n");  // ขึ้นคำว่า C Tokens!
-    return 0;               // บอกว่าโปรแกรมทำงานเสร็จสมบูรณ์
-}
