c806294b6e1173a5c69cb2002550343a68f1a2f5
Thanachod-6812022611308 <s6812022611308@email.kmutnb.ac.th>
2025-07-22 15:19:58 +0700

    Add files via upload

---
+++ coding_w04-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>          //input/output
+
+int main() {                // จุดเริ่มต้นโปรแกรม
+    printf("Hello,\n");     // ขึ้นคำว่า Hello
+    printf("C Tokens!\n");  // ขึ้นคำว่า C Tokens!
+    return 0;               // บอกว่าโปรแกรมทำงานเสร็จสมบูรณ์
+}
